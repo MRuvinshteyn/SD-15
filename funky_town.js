46c5c907235ac396574f1b5955a93eb23b54b708
Michael Ruvinshteyn <mruvinshteyn@stuy.edu>
2017-12-06 13:16:05 -0500

    added comments

--- funky_town.js
+++ funky_town.js
@@ -1,6 +1,13 @@
+//Michael Ruvinshteyn && Khyber Sen
+//SoftDev1 pd7
+//HW15 -- Sequential Progression
+//2017 - 12 - 06
+
 const fibonacci = function(n) {
+    //sets the first two numbers to be used for further computation
     let a = 0;
     let b = 1;
+    //loops until function reaches nth number in sequence
     for (let i = 0; i < n; i++) {
         const temp = b;
         b = a + b;
@@ -10,10 +17,13 @@ const fibonacci = function(n) {
 };
 
 const gcd = function(a, b) {
+    //checks if second number is 0 -> return first number
+    //otherwise return gcd of the second number and the remainder of f/s (a/b)
     return b === 0 ? a : gcd(b, a % b);
 };
 
 const randomStudent = function(students) {
+    //returns student at random index of a given list
     return students[Math.floor(Math.random() * students.length)];
 };
 
