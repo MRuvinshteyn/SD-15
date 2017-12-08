17969f6cc5caafedc1061e348aad061d9e159b42
Michael Ruvinshteyn <mruvinshteyn@stuy.edu>
2017-12-07 20:19:06 -0500

    moved class-friendly version into main directory (made khyber'd version folder

--- funky_town.js
+++ funky_town.js
@@ -1,106 +1,69 @@
-(function(window, test) {
-    "use strict";
-    /*
-     * Michael Ruvinshteyn and Khyber Sen
-     * HW 15 -- Sequential Progression
-     * 2017-12-06
-     */
+//Michael Ruvinshteyn && Khyber Sen
+//SoftDev1  pd7
+//HW 15 -- Sequential Progression
+//2017 - 12 - 07
 
-    const fibonacci = window.fibonacci = function(n) {
-        // sets the first two numbers to be used for further computation
-        let a = 0;
-        let b = 1;
-        // loops until function reaches nth number in sequence
-        for (let i = 0; i < n; i++) {
-            const temp = b;
-            b = a + b;
-            a = temp;
-        }
-        return a;
-    };
+var fibonacci = function(n) {
+    //two variables used for the first two numbers, 0 and 1
+	let a = 0;
+	let b = 1;
+	for (let i = 0; i < n; i++) {
+		const temp = b;
+		b = a + b;
+        //modify a to the new fibonacci number
+		a = temp;
+	}
+    //return the final fibonacci number
+	return a;
+};
 
-    const gcd = window.gcd = function(a, b) {
-        // checks if second number is 0 -> return first number
-        // otherwise return gcd of the second number and the remainder of f/s (a/b)
-        while (b !== 0) {
-            const temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    };
+var gcd = function(a,b) {
+    //if the second number is equal to 0, return the first number
+	if (b == 0){
+		return a;
+	};
+    
+    //otherwise, take the remainder of the the two numbers, and test gcd with that and the second number
+	return gcd(b, a % b);
+};
 
-    const randomStudent = window.randomStudent = function(students) {
-        // returns student at random index of a given list
-        return students[Math.floor(Math.random() * students.length)];
-    };
+var list = ["Michael","Khyber","Ruvinshteyn","Sen"];
+var random_student_selector = function() {
+    //return a string from a random index of the list
+	return list[Math.floor(Math.random() * list.length)];
+};
 
-    Array.prototype.indices = function() {
-        return [...this.keys()];
-    };
+//Method for next three functions:
+//1. Take the element from the inputs
+//2. Create a new paragraph to print the answer in
+//3. Take the result of the function with the given input
+//4. Append the result to the paragraph
+//5. Place the paragraph into the page
 
-    Object.prototype.print = function() {
-        console.log(this);
-    };
+function fibonacciIn() {
+	var elem = document.getElementById('fibIn').value;
+    var para = document.createElement('p');
+    var result = document.createTextNode("The " + elem + "th fibonacci number is " + fibonacci(elem));
+    para.appendChild(result);
+    document.getElementById('fib').appendChild(para);
+	console.log(elem + " ==> " + fibonacci(elem));
+};
 
-    if (test) {
-        const testFibonacci = function(n) {
-            console.log("Testing fibonacci:");
-            Array(n).indices().map(
-                n => console.log("The " + n + "th fibonacci number is: " + fibonacci(n))
-            );
-            console.log();
-        };
+function gcdIn() {
+    var elem1 = document.getElementById('gcd1').value;
+    var elem2 = document.getElementById('gcd2').value;
+    var para = document.createElement('p');
+    var result = document.createTextNode("GCD of " + elem1 + " and " + elem2 + ": " + gcd(elem1,elem2));
+    para.appendChild(result);
+    document.getElementById('gcd').appendChild(para);
+    console.log(elem1 + ", " + elem2 + " ==> " + gcd(elem1,elem2));
+}
 
-        const testGcd = function(a, b) {
-            console.log("Testing gcd:");
-            console.log("gcd(" + a + ", " + b + ") = " + gcd(a, b));
-            console.log();
-        };
-
-        const testRandomStudent = function(students, n) {
-            console.log("Testing randomStudent:");
-            console.log("Choosing " + n + " random students from [" + students.join(", ") + "]");
-            for (let i = 0; i < n; i++) {
-                console.log("Random student #" + i + ": " + randomStudent(students));
-            }
-            console.log();
-        };
-
-        testFibonacci(100);
-
-        testGcd(5678, 680);
-        console.log("We decided functions count as students, too.");
-        testRandomStudent([
-            "Khyber",
-            "Sen",
-            "Michael",
-            "Ruvinshteyn",
-            fibonacci,
-            gcd,
-            randomStudent,
-        ], 10);
-
-        if (typeof alert !== "undefined") {
-            alert("Done running tests!\nCheck the console for results.");
-        }
-    } else {
-        const testFibonacci = () => fibonacci(10);
-        const testGcd = () => gcd(5678, 680);
-        const testRandomStudent = () => randomStudent(["Khyber", "Sen", "Michael", "Ruvinshteyn"]);
-        [testFibonacci, testGcd, testRandomStudent].forEach(func => {
-            const div = document.createElement('div');
-            document.body.appendChild(div);
-            const button = document.createElement('button');
-            div.appendChild(button);
-            button.innerText = func.toString().slice(6);
-            button.addEventListener('click', (event) => {
-                // event.print();
-                const result = document.createElement('p');
-                div.appendChild(result);
-                result.innerText = func().toString();
-            });
-            new Array(3).fill(0).forEach(() => document.body.appendChild(document.createElement('br')));
-        });
-    }
-})(window, false);
+function random_studentIn() {
+    var para = document.createElement('p');
+    var ret = random_student_selector();
+    var result = document.createTextNode("Random student from given list: " + ret);
+    para.appendChild(result);
+    document.getElementById('random_student').appendChild(para);
+    console.log("Random student from given list: " + ret);
+}
