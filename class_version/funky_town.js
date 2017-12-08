63354bf034dcef05aaa4d9ef760bf818180b35a8
Michael Ruvinshteyn <mruvinshteyn@stuy.edu>
2017-12-07 19:29:57 -0500

    C O M M E N T S

--- class_version/funky_town.js
+++ class_version/funky_town.js
@@ -4,27 +4,41 @@
 //2017 - 12 - 07
 
 var fibonacci = function(n) {
+    //two variables used for the first two numbers, 0 and 1
 	let a = 0;
 	let b = 1;
 	for (let i = 0; i < n; i++) {
 		const temp = b;
 		b = a + b;
+        //modify a to the new fibonacci number
 		a = temp;
 	}
+    //return the final fibonacci number
 	return a;
 };
 
 var gcd = function(a,b) {
+    //if the second number is equal to 0, return the first number
 	if (b == 0){
 		return a;
 	};
+    
+    //otherwise, take the remainder of the the two numbers, and test gcd with that and the second number
 	return gcd(b, a % b);
 };
 
 var list = ["Michael","Khyber","Ruvinshteyn","Sen"];
 var random_student_selector = function() {
+    //return a string from a random index of the list
 	return list[Math.floor(Math.random() * list.length)];
 };
+
+//Method for next three functions:
+//1. Take the element from the inputs
+//2. Create a new paragraph to print the answer in
+//3. Take the result of the function with the given input
+//4. Append the result to the paragraph
+//5. Place the paragraph into the page
 
 function fibonacciIn() {
 	var elem = document.getElementById('fibIn').value;
